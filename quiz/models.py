b185b7e17b8e6d60da1de9e2a00767f8c59acbd2
Sabi Ruckova <sabinka.ruckova@gmail.com>
2026-06-03 09:28:18 +0200

    s

--- quiz/models.py
+++ quiz/models.py
@@ -16,11 +16,13 @@ class Profil(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profil')
     vek = models.PositiveIntegerField(null=True, blank=True)
     is_teacher = models.BooleanField(default=False)
-    # Teraz už Python vie, čo je 'Trieda'
     trieda = models.ForeignKey(Trieda, on_delete=models.SET_NULL, null=True, blank=True, related_name='studenti')
 
     def __str__(self):
-        return f"{self.user.username}"
+        # Ak má vyplnené meno alebo priezvisko, vráti ich spojené. Inak vráti prihlasovacie meno.
+        if self.user.first_name or self.user.last_name:
+            return f"{self.user.first_name} {self.user.last_name}".strip()
+        return self.user.username
     
 
 class Question(models.Model):
@@ -73,12 +75,18 @@ class TestQuestion(models.Model):
         ordering = ['order']
 
 class Result(models.Model):
-    # Odporúčam odkazovať na originál Usera kvôli autentifikácii
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     test = models.ForeignKey('Test', on_delete=models.CASCADE)
     score = models.IntegerField()
     percentage = models.DecimalField(max_digits=5, decimal_places=2)
     completed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # Tu sa namiesto škaredého username zobrazí pekné meno študenta pri výsledku testu
+        if self.user.first_name or self.user.last_name:
+            full_name = f"{self.user.first_name} {self.user.last_name}".strip()
+            return f"{full_name} - {self.test.title}"
+        return f"{self.user.username} - {self.test.title}"
 
     
 class StudentResponse(models.Model):
